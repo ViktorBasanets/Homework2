c1e0c7453971bffe30ad3ea157401597e5517ad4
ViktorBasanets <vbasanec@gmail.com>
2018-05-17 08:54:13 +0300

    Corrections

--- src/Task2/ReverseSubstring.java
+++ src/Task2/ReverseSubstring.java
@@ -4,37 +4,41 @@ public class ReverseSubstring {
     public static void main(String[] args) {
 
         char[] array = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
-        int from = 2, to = 4;
+        int from = -1, to = 6;
 
-        try {
-            System.out.print(array);
-            System.out.print(" - substring \"");
-            showSubstring(array, from, to);
-            System.out.print("\" was reversed to \"");
-            showReversedSubstring(array, from, to);
-            System.out.print("\"");
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
+        System.out.print(array);
+        showSubstring(array, from, to);
+        showReversedSubstring(array, from, to);
+        System.out.print("\"");
     }
 
-    static void showSubstring(char[] array, int from, int to) {
+    private static void showSubstring(char[] array, int from, int to) {
 
         if(from < 0 || from > to || to >= array.length) {
-            throw new IndexOutOfBoundsException();
+
+            System.out.println();
+            System.err.println("Error: Outside the array!\n" +
+                    "Array boundaries: (0, " + (array.length - 1) + ")");
+            System.exit(-1);
         }
 
+        System.out.print(" - substring \"");
         for(int i = from; i <= to; i++) {
             System.out.print(array[i]);
         }
     }
 
-    static void showReversedSubstring(char[] array,
+    private static void showReversedSubstring(char[] array,
                                       int from, int to) {
         if(from < 0 || from > to || to >= array.length) {
-            throw new IndexOutOfBoundsException();
+
+            System.out.println();
+            System.err.println("Error: Outside the array!\n" +
+                    "Array boundaries: (0, " + (array.length - 1) + ")");
+            System.exit(-1);
         }
 
+        System.out.print("\" was reversed to \"");
         for(int i = to; i >= from; i--) {
             System.out.print(array[i]);
         }
